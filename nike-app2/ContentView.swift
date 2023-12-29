9810abad1e3a5a709eccddff6551294a9f8d63c4
Darya <drsarya043@gmail.com>
2023-12-29 00:25:51 -0800

    Initial Commit

---
+++ nike-app2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nike-app2
+//
+//  Created by darya on 29.12.2023.
+//  Copyright © 2023 darya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
